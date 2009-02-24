c0f8270d10aed024a4bf5317420f79f1ea03dc69
Sarah Sharp <sarah.a.sharp@intel.com>
2009-02-24 14:01:20 -0800

    Add a common exit routine.
    Add a function that will be called whenever the script calls exit.  This
    function will clean up any temporary files, and handle putting the USB sysfs
    files back in order if something failed during the test.  The function will also
    append the VID:PID to /etc/usb-pm-tool/pm-broken-devices.txt if the user reports
    the device doesn't work, or the device disconnected during the test.  (I'm
    assuming it disconnected because of the selective suspend command).
    
    Signed-off-by: Sarah Sharp <sarah.a.sharp@intel.com>

--- usb-pm-tool.sh
+++ usb-pm-tool.sh
@@ -19,6 +19,43 @@
 
 # Author: Sarah Sharp <sarah.a.sharp@linux.intel.com>
 
+cleanup ()
+{
+	if test -e "$DEVS_FILE"; then
+		rm "$DEVS_FILE" 
+	fi
+	if [ -e "$PM_DEVS_FILE" ]; then
+		rm "$PM_DEVS_FILE" 
+	fi
+	if [ -e "$NO_PM_DEVS_FILE" ]; then
+		rm "$NO_PM_DEVS_FILE" 
+	fi
+	# Always put the parent roothub back in the state we found it in.
+	if [ -e "$PARENT" -a "$OLD_PARENT_LEVEL" != "" ]; then
+		echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
+	fi
+	# If the test failed and the device is still connected,
+	# put it back in the state we found it.
+	if [ "$TEST_SUCCESS" = "0" -a -d "$SYSFS_DIR" -a "$OLD_LEVEL" != "" ]; then
+		echo $OLD_WAIT > "$SYSFS_DIR/power/autosuspend"
+		echo $OLD_LEVEL > "$SYSFS_DIR/power/level"
+	fi
+	# If the device disconnected when it was told to auto-suspend,
+	# or the user reports it's unusable, record that for later use.
+	if [ "$BROKEN_DEVICE" = "1" ]; then
+		echo "Not enabling auto-suspend udev rule."
+		echo "Appending VID:PID $VID:$PID to $UNSAFE_DEVS_FILE"
+		# Avoid duplicate entries
+		if ! grep -q "$VID:$PID" $UNSAFE_DEVS_FILE; then
+			echo "$VID:$PID" >> $UNSAFE_DEVS_FILE
+		fi
+		# FIXME make a bug report - might be something to do with the driver?
+	fi
+}
+
+TEST_SUCCESS=0
+BROKEN_DEVICE=0
+trap cleanup EXIT
 
 echo ""
 echo "USB Power Management Tool v 0.1"
@@ -244,20 +281,14 @@ PARENT_TIME=$(cat "$PARENT/power/active_duration")
 # Be paranoid at this point about files, because the device might break and the
 # files might go away.  FIXME this should probably be a function...
 if ! TIME=$(cat "$SYSFS_DIR/power/active_duration"); then
-	echo "Device died?  Not enabling auto-suspend udev rule."
-	# Clean up
-	echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
-	# FIXME - offer to send a message that the device died?
+	BROKEN_DEVICE=1
 	exit 1
 fi
 
 sleep 0.2
 PARENT_TIME2=$(cat "$PARENT/power/active_duration")
 if ! TIME2=$(cat "$SYSFS_DIR/power/active_duration"); then
-	echo "Device died?  Not enabling auto-suspend udev rule."
-	# Clean up
-	echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
-	# FIXME - offer to send a message that the device died?
+	BROKEN_DEVICE=1
 	exit 1
 fi
 
@@ -267,13 +298,8 @@ fi
 # Put in a slight buffer
 if [ $(($TIME2 - $TIME)) -ge $((($PARENT_TIME2 - $PARENT_TIME) * 7 / 8)) ]; then
 	echo "Device still active, test inconclusive."
-	# Clean up
-	echo $OLD_WAIT > "$SYSFS_DIR/power/autosuspend"
-	echo $OLD_LEVEL > "$SYSFS_DIR/power/level"
-	echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
 	exit 1
 fi
-
 
 # Now test to see if the device correctly wakes up.
 
@@ -292,15 +318,7 @@ echo
 # This way the user won't need to be actively using the device when they
 # press enter.  FIXME: this is copy-paste code, make a function!
 if ! URB_NUM1=$(cat "$SYSFS_DIR/urbnum"); then
-	echo "Device died?  Not enabling auto-suspend udev rule."
-	echo "Appending VID:PID $VID:$PID to $UNSAFE_DEVS_FILE"
-	# Avoid duplicate entries
-	if ! grep -q "$VID:$PID" $UNSAFE_DEVS_FILE; then
-		echo "$VID:$PID" >> $UNSAFE_DEVS_FILE
-	fi
-	# Clean up
-	echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
-	# FIXME - offer to send a message that the device died?
+	BROKEN_DEVICE=1
 	exit 1
 fi
 
@@ -314,24 +332,12 @@ read ignored
 echo
 
 if ! URB_NUM2=$(cat "$SYSFS_DIR/urbnum"); then
-	echo "Device died?  Not enabling auto-suspend udev rule."
-	echo "Appending VID:PID $VID:$PID to $UNSAFE_DEVS_FILE"
-	# Avoid duplicate entries
-	if ! grep -q "$VID:$PID" $UNSAFE_DEVS_FILE; then
-		echo "$VID:$PID" >> $UNSAFE_DEVS_FILE
-	fi
-	# Clean up
-	echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
-	# FIXME - offer to send a message that the device died?
+	BROKEN_DEVICE=1
 	exit 1
 fi
 
 if [ $URB_NUM1 -eq $URB_NUM2 ]; then
 	echo "Device still suspended, test inconclusive."
-	# Clean up
-	echo $OLD_WAIT > "$SYSFS_DIR/power/autosuspend"
-	echo $OLD_LEVEL > "$SYSFS_DIR/power/level"
-	echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
 	exit 1
 fi
 
@@ -342,50 +348,36 @@ echo -n "Device successfully resumed.  Does this device still work? (y/n): "
 read working
 echo ""
 if [ "$working" != 'y' -a  "$working" != 'Y' -a  "$working" != 'yes'  -a  "$working" != 'Yes' -a "$working" != 'YES' ]; then
-	SUCCESS=0
 	echo "What was wrong with the device: "
 	read notes
 	echo ""
-	# FIXME make a bug report - might be something to do with the driver?
-	# If the device didn't suspend properly, clean up the device pm files
-	# and make it so the device will always be on.
-	echo "Disabling auto-suspend for this device."
-	echo $OLD_WAIT > "$SYSFS_DIR/power/autosuspend"
-	echo "on" > "$SYSFS_DIR/power/level"
-	echo "Appending VID:PID $VID:$PID to $UNSAFE_DEVS_FILE"
-	# Avoid duplicate entries
-	if ! grep -q "$VID:$PID" $UNSAFE_DEVS_FILE; then
-		echo "$VID:$PID" >> $UNSAFE_DEVS_FILE
-	fi
-else
-	SUCCESS=1
-	echo "You can enable auto-suspend for this device whenever it is"
-	echo "plugged into the system.  This step will append the device's"
-	echo "vendor and product ID to $SAFE_DEVS_FILE"
-	echo "and regenerate udev rules in /etc/udev/rules.d/025_usb-autosuspend.rules."
-	# FIXME Josh wants to see the rule that would be added
-	echo
-	echo "Do you want to always enable auto-suspend for this device?"
-	echo -n "This will decrease system power consumption.  (y/n): "
-	read rule
-	echo ""
-	if [ "$rule" = 'y' -o  "$rule" = 'Y' -o  "$rule" = 'yes'  -o  "$rule" = 'Yes' -o "$rule" = 'YES' ]; then
-		# Avoid duplicate entries
-		echo "Appending VID:PID $VID:$PID to $SAFE_DEVS_FILE"
-		echo
-		if ! grep -q "$VID:$PID" $SAFE_DEVS_FILE; then
-			echo "$VID:$PID" >> $SAFE_DEVS_FILE
-		fi
-		if ! ./vid-pid-to-udev-rule.sh $SAFE_DEVS_FILE; then
-			echo "WARNING: could not regenerate udev rules."
-			echo "Is vid-pid-to-udev-rule.sh in the current directory?"
-		fi
-	fi
+	BROKEN_DEVICE=1
+	exit 1
 fi
 
-# Clean up the root hub's files we messed with
-echo $OLD_PARENT_LEVEL > "$PARENT/power/level"
-
+TEST_SUCCESS=1
+echo "You can enable auto-suspend for this device whenever it is"
+echo "plugged into the system.  This step will append the device's"
+echo "vendor and product ID to $SAFE_DEVS_FILE"
+echo "and regenerate udev rules in /etc/udev/rules.d/025_usb-autosuspend.rules."
+# FIXME Josh wants to see the rule that would be added
+echo
+echo "Do you want to always enable auto-suspend for this device?"
+echo -n "This will decrease system power consumption.  (y/n): "
+read rule
+echo ""
+if [ "$rule" = 'y' -o  "$rule" = 'Y' -o  "$rule" = 'yes'  -o  "$rule" = 'Yes' -o "$rule" = 'YES' ]; then
+	# Avoid duplicate entries
+	echo "Appending VID:PID $VID:$PID to $SAFE_DEVS_FILE"
+	echo
+	if ! grep -q "$VID:$PID" $SAFE_DEVS_FILE; then
+		echo "$VID:$PID" >> $SAFE_DEVS_FILE
+	fi
+	if ! ./vid-pid-to-udev-rule.sh $SAFE_DEVS_FILE; then
+		echo "WARNING: could not regenerate udev rules."
+		echo "Is vid-pid-to-udev-rule.sh in the current directory?"
+	fi
+fi
 
 # Ask user if they want to send an HTTP post report.  Tell them their IP address
 # will not be used to identify which USB devices they own.
